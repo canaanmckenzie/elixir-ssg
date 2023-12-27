e8c14d88dd1bb5d26d283572dd56cdc88d7e0403
Canaan <cjmckenz@uvm.edu>
2023-12-27 12:39:29 -0500

    Make it so

---
+++ lib/quant_notes.ex
@@ -0,0 +1,18 @@
+defmodule QuantNotes do
+  @moduledoc """
+  Documentation for `QuantNotes`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> QuantNotes.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
